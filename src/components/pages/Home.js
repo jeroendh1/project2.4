89c41a9f31f5f3102365df23ed9fdc0508916a0b
Jeroendh <jer.de.haan@st.hanze.nl>
2022-05-25 22:32:16 +0200

    !

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,43 +1,91 @@
 import React from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
-import Map from '../../components/map.js';
+import Map from "../../components/map.js";
+var top10 = "";
+let stations = JSON.parse(localStorage.getItem("stations"));
+let top10WindSpeeds = stations.sort(
+  (a, b) => parseFloat(b.wind_speed) - parseFloat(a.wind_speed)
+);
+top10WindSpeeds.length = 10;
 
-
-
+for (const top10WindSpeed of top10WindSpeeds) {
+  top10 =
+    top10 +
+    '<p><span class="ms-2 float-start">' +
+    top10WindSpeed.location +
+    ",  " +
+    top10WindSpeed.country +
+    '</span><span class="ms-4 float-end"> ' +
+    top10WindSpeed.wind_speed +
+    " km/h </span></p><hr>";
+}
 
 function Home() {
-  
-
   return (
     <div className="Home">
       <main>
         <Container>
           <Row>
             <Col className="mb-4" sm={12} md={8} lg={9}>
-            <Map/>
+              <Map />
+              <Row>
+                <Col className="mt-4" sm={0} md={4}>
+                  <Card className="center ">
+                    <Card.Body>
+                      <Card.Title>Highest wind speed</Card.Title>
+                      <Card.Subtitle className="mb-4 text-muted">
+                        In Arabic peninsula
+                      </Card.Subtitle>
+                      <h6
+                        className="mt-4"
+                        dangerouslySetInnerHTML={{
+                          __html:
+                            top10WindSpeeds[0].location +
+                            ", " +
+                            top10WindSpeeds[0].country +
+                            " " +
+                            top10WindSpeeds[0].wind_speed +
+                            "Km/h",
+                        }}
+                      ></h6>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col className="mt-4" md={4}>
+                  <Card className="center ">
+                    <Card.Body>
+                      <Card.Title>Highest humidity</Card.Title>
+                      <Card.Subtitle className="mb-4 text-muted">
+                        In Arabic peninsula
+                      </Card.Subtitle>
+                      <h6 className="mt-4">Badiyah, Oman 76% </h6>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col className="mt-4"  md={4}>
+                  <Card className="center ">
+                    <Card.Body>
+                      <Card.Title>Total number of stations</Card.Title>
+                      <Card.Subtitle className="mb-4 text-muted">
+                        In Arabic peninsula
+                      </Card.Subtitle>
+                      <h6
+                        dangerouslySetInnerHTML={{
+                          __html:  JSON.parse(localStorage.getItem("stations")).length}}
+                      ></h6>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
             </Col>
             <Col sm={12} md={4} lg={3}>
-              <Card className="center map-container">
+              <Card className="center ">
                 <Card.Body>
                   <Card.Title>Top 10 Windiest Places</Card.Title>
                   <Card.Subtitle className="mb-4 text-muted">
                     In Arabic peninsula
                   </Card.Subtitle>
-                  1.<span className="ms-2">Oman</span>
-                  <span className="ms-4"> 80 km/h</span>
-                  <hr></hr>
-                  2.<span className="ms-2">Jemen</span>
-                  <span className="ms-4"> 75 km/h</span>
-                  <hr></hr>
-                  3.<span className="ms-2">Saudi-Arabië</span>
-                  <span className="ms-4"> 69 km/h</span>
-                  <hr></hr>
-                  4.<span className="ms-2">Qatar</span>
-                  <span className="ms-4"> 58 km/h</span>
-                  <hr></hr>
-                  5.<span className="ms-2">Iran</span>
-                  <span className="ms-4"> 57 km/h</span>
-                  <hr></hr>
+                  <div id="top10" dangerouslySetInnerHTML={{ __html: top10 }} />
                 </Card.Body>
               </Card>
             </Col>
