4db877701b06c7d810c781aafed3e1c092c3930e
Jeroendh <jer.de.haan@st.hanze.nl>
2022-06-06 15:00:19 +0200

    f

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,51 +1,70 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import axios from "axios";
+import { Container, Row, Col, Card } from "react-bootstrap";
 export default class Register extends Component {
-    handleSubmit(event) {
-        const api_base = 'http://localhost:3001'
-        const password = event.target.password.value;
-        const email = event.target.email.value;
-        const reactData = { email: email, password: password};
-        axios.post(api_base + '/login', reactData)
-            .then(function (response) {
-                if (response.data.redirect === '/'){
-                    window.location = '/'
-                }
-            })
-            .catch(function(error) {
-                console.log(error);
-            })
-    }
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <h3>Sign In</h3>
-                <div className="mb-3">
-                    <label>Email address</label>
-                    <input
-                        name="email"
-                        type="email"
-                        id="email"
-                        className="form-control"
-                        placeholder="Enter email"
-                    />
-                </div>
-                <div className="mb-3">
-                    <label>Password</label>
-                    <input
-                        name="password"
-                        type="password"
-                        id="password"
-                        className="form-control"
-                        placeholder="Enter password"
-                    />
-                </div>
-                <div className="d-grid">
-                    <button type="submit" className="btn btn-primary">
-                        Submit
-                    </button>
-                </div>
-            </form>
-        )
-    }
+  handleSubmit(event) {
+    const api_base = "http://localhost:3001";
+    const password = event.target.password.value;
+    const email = event.target.email.value;
+    const reactData = { email: email, password: password };
+    axios
+      .post(api_base + "/login", reactData)
+      .then(function(response) {
+        if (response.data.redirect === "/") {
+          window.location = "/";
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+  render() {
+    return (
+      <div className="Login">
+        <main>
+          <Container>
+            <Row className="justify-content-md-center">
+              <Col className="mb-4" sm={12} md={6} lg={6}>
+                <Card className="center ">
+                  <Card.Body>
+                    <Card.Title>Sign In</Card.Title>
+                    <Card.Subtitle className="mb-4 text-muted">
+                    
+                    </Card.Subtitle>
+                    <form onSubmit={this.handleSubmit}>
+                      <div className="mb-3">
+                        <label>Email address</label>
+                        <input
+                          name="email"
+                          type="email"
+                          id="email"
+                          className="form-control"
+                          placeholder="Enter email"
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <label>Password</label>
+                        <input
+                          name="password"
+                          type="password"
+                          id="password"
+                          className="form-control"
+                          placeholder="Enter password"
+                        />
+                      </div>
+                      <div className="d-grid">
+                        <button type="submit" className="btn btn-primary">
+                          Submit
+                        </button>
+                      </div>
+                    </form>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </main>
+      </div>
+    );
+  }
 }
--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,50 +1,70 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import axios from "axios";
+import { Container, Row, Col, Card } from "react-bootstrap";
 export default class Register extends Component {
-    handleSubmit(event) {
-        const api_base = 'http://localhost:3001'
-        const password = event.target.password.value;
-        const email = event.target.email.value;
-        const reactData = { email: email, password: password};
-        axios.post(api_base + '/register', reactData)
-            .then((response) => {
-                alert(response)
-            console.log(response);
-        }, (error) => {
-                alert(error)
-            console.log(error);
-        });
-    }
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <h3>Register a Employee</h3>
-                <div className="mb-3">
-                    <label>Email address</label>
-                    <input
-                        name="email"
-                        type="email"
-                        id="email"
-                        className="form-control"
-                        placeholder="Enter email"
-                    />
-                </div>
-                <div className="mb-3">
-                    <label>Password</label>
-                    <input
-                        name="password"
-                        type="password"
-                        id="password"
-                        className="form-control"
-                        placeholder="Enter password"
-                    />
-                </div>
-                <div className="d-grid">
-                    <button type="submit" className="btn btn-primary">
-                        Submit
-                    </button>
-                </div>
-            </form>
-        )
-    }
+  handleSubmit(event) {
+    const api_base = "http://localhost:3001";
+    const password = event.target.password.value;
+    const email = event.target.email.value;
+    const reactData = { email: email, password: password };
+    axios.post(api_base + "/register", reactData).then(
+      (response) => {
+        alert(response);
+        console.log(response);
+      },
+      (error) => {
+        alert(error);
+        console.log(error);
+      }
+    );
+  }
+  render() {
+    return (
+      <div className="Register">
+        <main>
+          <Container>
+            <Row  className="justify-content-md-center">
+              <Col className="mb-4" sm={12} md={6} lg={6}>
+                <Card className="center ">
+                  <Card.Body>
+                    <Card.Title>Register a Employee</Card.Title>
+                    <Card.Subtitle className="mb-4 text-muted">
+                    
+                    </Card.Subtitle>
+                    <form onSubmit={this.handleSubmit}>
+                      <div className="mb-3">
+                        <label>Email address</label>
+                        <input
+                          name="email"
+                          type="email"
+                          id="email"
+                          className="form-control"
+                          placeholder="Enter email"
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <label>Password</label>
+                        <input
+                          name="password"
+                          type="password"
+                          id="password"
+                          className="form-control"
+                          placeholder="Enter password"
+                        />
+                      </div>
+                      <div className="d-grid">
+                        <button type="submit" className="btn btn-primary">
+                          Submit
+                        </button>
+                      </div>
+                    </form>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </main>
+      </div>
+    );
+  }
 }
